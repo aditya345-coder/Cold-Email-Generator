a3a7faa315a859a177e941913087f3a5d7ee152c
aditya345-coder <adityagaharwar55@gmail.com>
2024-09-05 14:17:19 +0530

    "added new project"

---
+++ app/chains.py
@@ -0,0 +1,61 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Aditya, a BTech computer science student at DIT University. I have experience in python, flask, django, html, css,
+            html, css, deep learning, machine learning, computer vision, NLP.
+            I have done 2 internship one in IIIT Dharwad where I annoted the data using CVAT and then created model for the correct unpaved road 
+            prediction with my colleague and second in a company called kohli LLP where we worked on time sereis data and trained the model using different 
+            time series models such as TFT, neuralprophet and hybird models etc.
+            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
+            in fulfilling their needs.
+            Also add the most relevant ones from the following links to showcase my portfolio: {link_list}
+            Remember you are Aditya. 
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
